dac9554823dbc91f3da6195106bdd4bc87f96c98
ShunSakurai <justmyway.yui@gmail.com>
2019-02-16 16:06:17 +0900

    Automatically reload Asana pages on install and update

--- background.js
+++ background.js
@@ -7,11 +7,18 @@ chrome.tabs.onUpdated.addListener(
   }
 );
 
-// Use only when an important update is made
-// chrome.runtime.onInstalled.addListener(
-//   function (details) {
-//     if (details.reason === 'update') {
-//       chrome.tabs.create({url: 'update.html'});
-//     }
-//   }
-// );
+chrome.runtime.onInstalled.addListener(
+  function (details) {
+    if (details.reason === 'install' || details.reason === 'update') {
+      chrome.tabs.query({url: 'https://app.asana.com/*'}, function (arrayOfTabs) {
+        arrayOfTabs.forEach(function (tab) {
+          chrome.tabs.reload(tab.id);
+        });
+      });
+    }
+    // Use only when an important update is made
+    // if (details.reason === 'update') {
+    //    chrome.tabs.create({url: 'update.html'});
+    //  }
+  }
+);
