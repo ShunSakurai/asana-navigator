a6baa1b3d3c23a75539f744c04d9dcf520a7bebe
ShunSakurai <justmyway.yui@gmail.com>
2019-04-29 00:33:55 +0900

    Do not display section selector on section in the right pane

--- content_script.js
+++ content_script.js
@@ -419,7 +419,7 @@ const displayProjectsOnTop = function () {
 
     // Section selectors as DOM elements are loaded later, so fetching them via API
     callAsanaApi('GET', `projects/${projectGid}/sections`, {}, {}, function (response) {
-      if (response.data.length) {
+      if (response.data.length && !response.data.map(section => section.gid).includes(taskGid)) {
         const taskAncestryAncestorProjectSectionSelector = document.createElement('A');
         taskAncestryAncestorProjectSectionSelector.setAttribute('class', 'NoBorderBottom FloatingSelect TaskAncestry-ancestorProject');
         taskAncestryAncestorProjectSectionSelector.innerHTML = '<svg class="Icon DownIcon FloatingSelect-icon" focusable="false" viewBox="0 0 32 32"><path d="M16,22.5c-0.3,0-0.7-0.1-0.9-0.3l-11-9c-0.6-0.5-0.7-1.5-0.2-2.1c0.5-0.6,1.5-0.7,2.1-0.2L16,19.1l10-8.2c0.6-0.5,1.6-0.4,2.1,0.2c0.5,0.6,0.4,1.6-0.2,2.1l-11,9C16.7,22.4,16.3,22.5,16,22.5z"></path></svg>';
